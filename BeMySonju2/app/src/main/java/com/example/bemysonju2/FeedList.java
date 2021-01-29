ddd17d6149731377c1fada09239b13a129df3485
Ohgyuchan <gyu3637@gmail.com>
2021-01-29 20:02:17 +0900

    Update FeedList

--- BeMySonju2/app/src/main/java/com/example/bemysonju2/FeedList.java
+++ BeMySonju2/app/src/main/java/com/example/bemysonju2/FeedList.java
@@ -2,7 +2,6 @@ package com.example.bemysonju2;
 
 import androidx.appcompat.app.AppCompatActivity;
 
-import android.content.Intent;
 import android.os.Bundle;
 
 public class FeedList extends AppCompatActivity {
@@ -10,7 +9,7 @@ public class FeedList extends AppCompatActivity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_help__feed);
+        setContentView(R.layout.activity_feed_list);
 
 
     }
