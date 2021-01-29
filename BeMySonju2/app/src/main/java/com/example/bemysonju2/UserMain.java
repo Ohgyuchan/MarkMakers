274a0ef2724db15e8e1bf7cf56f8a0b8a4630f5a
Ohgyuchan <gyu3637@gmail.com>
2021-01-29 20:10:46 +0900

    Intent 수정(UserMain -> FeedList

--- BeMySonju2/app/src/main/java/com/example/bemysonju2/UserMain.java
+++ BeMySonju2/app/src/main/java/com/example/bemysonju2/UserMain.java
@@ -22,7 +22,7 @@ public class UserMain extends AppCompatActivity {
         btn_fd.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
-                Intent intent = new Intent(UserMain.this, FeedWrite.class);
+                Intent intent = new Intent(UserMain.this, FeedList.class);
                 startActivity(intent);
             }
         });
