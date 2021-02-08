623a89329c530fecc6ae044c08b017bfe7bc42d8
Ohgyuchan <gyu3637@gmail.com>
2021-02-08 12:18:47 +0900

    screnn widget 폴더 생성

--- litening_letter/lib/main.dart
+++ litening_letter/lib/main.dart
@@ -1,83 +1,52 @@
 import 'package:flutter/material.dart';
+import 'package:litening_letter/widget/bottom_bar.dart';
 
-void main() {
-  runApp(MaterialApp(
-    title: '듣는 편지',
-    home: First(),
-  ));
+void main() => runApp(MyApp());
+
+class MyApp extends StatefulWidget {
+  _MyAppState createState() => _MyAppState();
 }
 
-class First extends StatelessWidget {
+class _MyAppState extends State<MyApp> {
+  TabController controller;
   @override
   Widget build(BuildContext context) {
-    return Center(
-      child: Column(
-        children: <Widget>[
-          RaisedButton(
-            child: Text('홈', style: TextStyle(fontSize: 50)),
-            color: Colors.white,
-            onPressed: () {
-              // push에 전달되는 두 번째 매개변수는 Route<T> 클래스.
-              Navigator.push(context,
-                  MaterialPageRoute<void>(builder: (BuildContext context) {
-                return Home();
-              }));
-            },
-          ),
-          RaisedButton(
-            child: Text('피드', style: TextStyle(fontSize: 50)),
-            color: Colors.white,
-            onPressed: () {
-              // push에 전달되는 두 번째 매개변수는 Route<T> 클래스.
-              Navigator.push(context,
-                  MaterialPageRoute<void>(builder: (BuildContext context) {
-                return Feed();
-              }));
-            },
-          ),
-        ],
-        mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+    return MaterialApp(
+      title: 'Gyufilx',
+      theme: ThemeData(
+        brightness: Brightness.dark,
+        primaryColor: Colors.black,
+        accentColor: Colors.white,
       ),
-    );
-  }
-}
-
-class Home extends StatelessWidget {
-  @override
-  Widget build(BuildContext context) {
-    return Scaffold(
-      appBar: AppBar(title: Text('홈')),
-      body: Center(
-        child: RaisedButton(
-          child: Text('처음 화면으로 돌아가기', style: TextStyle(fontSize: 21)),
-          color: Colors.white,
-          onPressed: () {
-            Navigator.pop(context);
-
-// 위와 같은 코드
-// Navigator.of(context).pop();
-          },
-        ),
-      ),
-    );
-  }
-}
-
-class Feed extends StatelessWidget {
-  @override
-  Widget build(BuildContext context) {
-    return Scaffold(
-      appBar: AppBar(title: Text('피드')),
-      body: Center(
-        child: RaisedButton(
-          child: Text('처음 화면으로 돌아가기', style: TextStyle(fontSize: 21)),
-          color: Colors.white,
-          onPressed: () {
-            Navigator.pop(context);
-
-            // 위와 같은 코드
-            // Navigator.of(context).pop();
-          },
+      home: DefaultTabController(
+        length: 4,
+        child: Scaffold(
+          body: TabBarView(
+            physics: NeverScrollableScrollPhysics(),
+            children: <Widget>[
+              Container(
+                child: Center(
+                  child: Text('center'),
+                ),
+              ),
+              Container(
+                child: Center(
+                  child: Text('serch'),
+                ),
+              ),
+              Container(
+                child: Center(
+                  child: Text('save'),
+                ),
+              ),
+              Container(
+                child: Center(
+                  child: Text('list'),
+                ),
+              ),
+            ],
+          ),
+          bottomNavigationBar: Bottom(),
         ),
       ),
     );
